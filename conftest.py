b4bc8cf4071c0bf8cf43b4a151eef47a3420e654
Meghan Pund <meghan.pund@gmail.com>
2021-10-12 11:29:34 -0400

    I think this is back to normal

--- conftest.py
+++ conftest.py
@@ -15,26 +15,6 @@ from mongoquery import Query
 from tests.nodes import convert_node, flatten
 from tests.template import Template
 
-start_time = None
-total_written = 0
-
-@hooks.register("start_build")
-def start_build():
-    global start_time
-    start_time = time.localtime
-
-@hooks.register("written")
-def written():
-    global total_written
-    total_written += 1
-
-@hooks.register("stats")
-def stats():
-    final_time = time.localtime - start_time
-    average = final_time / total_written if not 0 else 0
-    report = "Converted: {} · Time: {:.2f} sec · Avg: {:.4f} sec/file"
-    print(report.format(total_written, final_time, average))
-
 class Parser:
     def __init__(self, file_name, nodes):
 
