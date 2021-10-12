79b2e906dc736f33983643a1e020764db10d2a82
Meghan Pund <meghan.pund@gmail.com>
2021-10-12 11:17:30 -0400

    pushing

--- ssg/site.py
+++ ssg/site.py
@@ -1,5 +1,6 @@
 import sys
 from pathlib import Path
+from conftest import start_build, stats
 from ssg import extensions, hooks
 
 
@@ -30,12 +31,14 @@ class Site:
     def build(self):
         extensions.load_bundled()
         hooks.event("collect_files", self.source, self.parsers)
+        start_build
         self.dest.mkdir(parents=True, exist_ok=True)
         for path in self.source.rglob("*"):
             if path.is_dir():
                 self.create_dir(path)
             elif path.is_file():
                 self.run_parser(path)
+        stats
 
     @staticmethod
     def error(message):
