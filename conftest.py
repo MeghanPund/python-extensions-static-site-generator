6be04f59e0f62aafdaf80cae78428aa598827241
Meghan Pund <meghan.pund@gmail.com>
2021-10-12 11:16:22 -0400

    added colons to functions oops

--- conftest.py
+++ conftest.py
@@ -21,12 +21,12 @@ start_time = None
 total_written = 0
 
 @hooks.register("start_build")
-def start_build()
+def start_build():
     global start_time
     start_time = time.localtime
 
 @hooks.register("written")
-def written()
+def written():
     global total_written
     total_written += 1
 
